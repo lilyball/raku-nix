8268c405b4de20400807bd84444d760e74d5b267
Lily Ballard <lily@sb.org>
2020-12-02 13:51:01 -0800

    Fix up rakuWithPackages
    * Provide all rakudo binaries instead of just `raku` and `perl6`.
    * Use `,` instead of `:` when the calling environment already has
      `PERL6LIB`.
    * Set argv0 on the wrappers.
    * Inherit the `meta` attribute from rakudo.
    * Inherit `passthru` from rakudo.
    * Define `passthru.rakudo` to be the original rakudo derivation.
    * Define `passthru.env` as an environment for `nix-shell`.

--- tools/rakuWithPackages.nix
+++ tools/rakuWithPackages.nix
@@ -1,22 +1,47 @@
 { lib, makeWrapper, rakudo, rakuPackages, stdenvNoCC }: 
 getPs:
-stdenvNoCC.mkDerivation {
-    name = "${rakudo.name}-env";
-    buildInputs = [ makeWrapper ];
+let env = stdenvNoCC.mkDerivation {
+  name = "${rakudo.name}-env";
+  nativeBuildInputs = [ makeWrapper ];
+  inherit rakudo;
+  packages = getPs rakuPackages;
+  phases = [ "installPhase" ];
+  installPhase = ''
+    mkdir --parents $out/bin
+
+    PERL6LIB=
+    for p in $packages; do
+      PERL6LIB=$(< $p/PERL6LIB)''${PERL6LIB:+,$PERL6LIB}
+    done
+
+    for prg in $rakudo/bin/*; do
+      prgname=''${prg##*/}
+      if [ -f "$prg" ] && [ -x "$prg" ]; then
+        makeWrapper "$prg" $out/bin/"$prgname" \
+          --argv0 '$0' \
+          --prefix PERL6LIB , "$PERL6LIB"
+      else
+        ln -s "$prg" $out/bin/"$prgname"
+      fi
+    done
+  '';
+
+  inherit (rakudo) meta;
+
+  passthru = rakudo.passthru // {
+    # python's buildEnv provides an `interpreter` entry here that points to the python executable.
+    # I don't know why it does this, or whether we might benefit from doing the same.
     inherit rakudo;
-    phases = [ "installPhase" ];
-    installPhase = ''
-        mkdir --parents $out/bin
+    env = stdenvNoCC.mkDerivation {
+      name = "interactive-${rakudo.name}-environment";
+      nativeBuildInputs = [ env ];
 
-        PERL6LIB=
-        for p in ${ lib.concatMapStringsSep " " (p: "${p}")
-                                            (getPs rakuPackages) }; do
-            PERL6LIB=$(< $p/PERL6LIB),$PERL6LIB
-        done
-
-        makeWrapper $rakudo/bin/raku $out/bin/raku \
-            --prefix PERL6LIB : "$PERL6LIB"
-        makeWrapper $rakudo/bin/perl6 $out/bin/perl6 \
-            --prefix PERL6LIB : "$PERL6LIB"
-    '';
-}
+      buildCommand = ''
+          echo >&2 ""
+          echo >&2 "*** Rakudo 'env' attributes are intended for interactive nix-shell sessions, not for building! ***"
+          echo >&2 ""
+          exit 1
+      '';
+    };
+  };
+}; in env
